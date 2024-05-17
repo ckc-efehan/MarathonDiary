1daf9fd38cd6c2f953405840575eca704b265ce9
Efehan Ömer Cekic <Efehan.Cekic@Student.HTW-Berlin.de>
2024-05-17 21:20:22 +0200

    Datum hinzugefügt

--- MarathonDiary-backend/src/main/java/de/htwberlin/webtech/MarathonTagebuch/MarathonDiary.java
+++ MarathonDiary-backend/src/main/java/de/htwberlin/webtech/MarathonTagebuch/MarathonDiary.java
@@ -1,18 +1,27 @@
 package de.htwberlin.webtech.MarathonTagebuch;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class MarathonDiary {
+    private LocalDate date;
     private double targetTime;
     private double targetKilometre;
     private double kilometreRan;
     private double timeRan;
     private boolean goalReached;
 
-    public MarathonDiary(double targetTime, double targetKilometre, double kilometreRan, double timeRan, boolean goalReached) {
+    public MarathonDiary(LocalDate date, double targetTime, double targetKilometre, double kilometreRan, double timeRan, boolean goalReached) {
+        this.date = date;
         this.targetTime = targetTime;
         this.targetKilometre = targetKilometre;
         this.kilometreRan = kilometreRan;
         this.timeRan = timeRan;
         this.goalReached = goalReached;
+    }
+
+    public LocalDate getDate() {
+        return date;
     }
 
     public double getTargetTime() {
--- MarathonDiary-backend/src/main/java/de/htwberlin/webtech/MarathonTagebuch/MyController.java
+++ MarathonDiary-backend/src/main/java/de/htwberlin/webtech/MarathonTagebuch/MyController.java
@@ -3,17 +3,18 @@ package de.htwberlin.webtech.MarathonTagebuch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 public class MyController {
     @GetMapping("/marathondiary")
     public List<MarathonDiary> Greeting () {
-        MarathonDiary entry = new MarathonDiary(2.5,5.0, 3.0, 5.0, false);
-        MarathonDiary entry1 = new MarathonDiary(2.0,6.0, 4.0, 3.0, false);
-        MarathonDiary entry2 = new MarathonDiary(4.0,10.0, 10.0, 3.5, true);
-        MarathonDiary entry3 = new MarathonDiary(3.0,12.0, 10.0, 5.0, false);
-        MarathonDiary entry4 = new MarathonDiary(5.0,12.0, 10.5, 5.0, false);
+        MarathonDiary entry = new MarathonDiary(LocalDate.of(2024,05,17), 2.5, 5.0, 3.0, 5.0, false);
+        MarathonDiary entry1 = new MarathonDiary(LocalDate.of(2024,05,15),2.0,6.0, 4.0, 3.0, false);
+        MarathonDiary entry2 = new MarathonDiary(LocalDate.of(2024,05,13),4.0,10.0, 10.0, 3.5, true);
+        MarathonDiary entry3 = new MarathonDiary(LocalDate.of(2024,05,11),3.0,12.0, 10.0, 5.0, false);
+        MarathonDiary entry4 = new MarathonDiary(LocalDate.of(2024,05,9),5.0,12.0, 10.5, 5.0, false);
         return List.of(entry, entry1, entry2, entry3, entry4);
     }
 }
