e66a341da9531676d75ddf37d25769678e7db087
Efehan Ömer Cekic <Efehan.Cekic@Student.HTW-Berlin.de>
2024-05-28 15:54:28 +0200

    mss

--- MarathonDiary-backend/src/main/java/de/htwberlin/webtech/MarathonTagebuch/MarathonTagebuchApplication.java
+++ MarathonDiary-backend/src/main/java/de/htwberlin/webtech/MarathonTagebuch/MarathonTagebuchApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MarathonTagebuchApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MarathonTagebuchApplication.class, args);
 	}
-
 }
