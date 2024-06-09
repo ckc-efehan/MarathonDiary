fa7789bf603420b49c96f2ada9571b32217fd8b5
Efehan Ömer Cekic <Efehan.Cekic@Student.HTW-Berlin.de>
2024-06-09 15:39:29 +0200

    Registrieren möglich v2

--- MarathonDiary-backend/src/main/java/de/htwberlin/webtech/MarathonTagebuch/Controller/AuthController.java
+++ MarathonDiary-backend/src/main/java/de/htwberlin/webtech/MarathonTagebuch/Controller/AuthController.java
@@ -3,6 +3,7 @@ package de.htwberlin.webtech.MarathonTagebuch.Controller;
 
 import de.htwberlin.webtech.MarathonTagebuch.Entities.UserEntity;
 import de.htwberlin.webtech.MarathonTagebuch.Interface.UserRepository;
+import de.htwberlin.webtech.MarathonTagebuch.Service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -17,9 +18,12 @@ public class AuthController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+
     @PostMapping("/register")
     public UserEntity register(@RequestBody UserEntity user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
+        return userDetailsService.save(user);
     }
 }
