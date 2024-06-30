ee03165c13f033b6f19773fa68ff6ab25e2e6df8
Efehan Ömer Cekic <Efehan.Cekic@Student.HTW-Berlin.de>
2024-06-30 09:42:41 +0200

    crossorigin changed

--- src/main/java/de/htwberlin/webtech/MarathonTagebuch/Controller/TrainingentriesController.java
+++ src/main/java/de/htwberlin/webtech/MarathonTagebuch/Controller/TrainingentriesController.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173", "https://marathondiary-frontend.onrender.com/"})
+@CrossOrigin(origins = { "https://marathondiary-frontend.onrender.com/", "http://localhost:5173"})
 @RequestMapping("/entries")
 public class TrainingentriesController {
 
